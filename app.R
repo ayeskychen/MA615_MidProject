c7c209b64fb101e034c9dd821ac51353226dbae4
Xiang Xu <xiangx7805@gmail.com>
2018-10-19 10:17:26 -0400

    lm modified

--- app.R
+++ app.R
@@ -193,7 +193,7 @@ server<-(function(input, output, session){
 
       if(input$show.line==TRUE)
       {
-        p<-p+ geom_smooth( se = FALSE)
+        p<-p+ geom_smooth(method = lm  ,se = FALSE)
       }
       
     }
